61d1bb4f0c3f881f7f08f2135913af49bbcf39ab
nathoalmeida <nathalia.oalmeida@gmail.com>
2026-06-03 14:06:02 -0300

    matriz de confusão, arvores plotadas

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score
+import matplotlib.pyplot as plt
+from sklearn.tree import export_graphviz
+import graphviz
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+
 
 colunas = [
     "id",
@@ -68,42 +74,149 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 ## ÁRVORES DE DECISÃO 
+arvores = [
+    ## Mais complexa
+{
+"nome": "Arvore 1",
+"criterion": "gini",
+"max_depth": None,
+"min_samples_leaf": 5,
+"min_samples_split": 2 
+},
+{
+"nome": "Arvore 2",
+"criterion": "gini",
+"max_depth": 5,
+"min_samples_leaf": 10,
+"min_samples_split": 5
+},
+{
+"nome": "Arvore 3",
+"criterion": "entropy",
+"max_depth": None,
+"min_samples_leaf": 10,
+"min_samples_split": 5
+},
+    ## Muito simples
+{
+"nome": "Arvore 4",
+"criterion": "entropy",
+"max_depth": 3,
+"min_samples_leaf": 20,
+"min_samples_split": 10
+}
+]
+
 resultados = []
 
-for criterion in ["gini", "entropy"]:
-    for max_depth in [3, 5, 7, None]:
-        for min_samples_leaf in [1, 5]:
-            for min_samples_split in [2, 10]:
+for config in arvores:
 
-                arvore = DecisionTreeClassifier(
-                    criterion=criterion,
-                    max_depth=max_depth,
-                    min_samples_leaf=min_samples_leaf,
-                    min_samples_split=min_samples_split,
-                    random_state=42
-                )
+    arvore = DecisionTreeClassifier(
+    criterion=config["criterion"],
+    max_depth=config["max_depth"],
+    min_samples_leaf=config["min_samples_leaf"],
+    min_samples_split=config["min_samples_split"],
+    random_state=42
+    )
 
-                arvore.fit(X_train, y_train)
+    arvore.fit(X_train, y_train)
 
-                y_pred = arvore.predict(X_test)
+    y_pred = arvore.predict(X_test)
 
-                resultados.append({
-                    "criterion": criterion,
-                    "max_depth": max_depth,
-                    "min_samples_leaf": min_samples_leaf,
-                    "min_samples_split": min_samples_split,
-                    "accuracy": accuracy_score(y_test, y_pred),
-                    "precision": precision_score(y_test, y_pred),
-                    "recall": recall_score(y_test, y_pred)
-                })
+    resultados.append({
+    "Árvore": config["nome"],
+    "Critério": config["criterion"],
+    "Profundidade": config["max_depth"],
+    "Min Leaf": config["min_samples_leaf"],
+    "Min Split": config["min_samples_split"],
+    "Accuracy": accuracy_score(y_test, y_pred),
+    "Precision": precision_score(y_test, y_pred),
+    "Recall": recall_score(y_test, y_pred)
+    })
 
 resultados_df = pd.DataFrame(resultados)
 
-print(
-    resultados_df.sort_values(
-        by="accuracy",
-        ascending=False
-    )
-)
+print(resultados_df)
 
-resultados_df.to_csv("resultados.csv", index=False)
+resultados_df.to_csv("resultados.csv", index=False) 
+
+## Compara resultados em um gráfico 
+
+resultados_df.set_index("Árvore")[
+["Accuracy", "Precision", "Recall"]
+].plot(kind="bar")
+
+plt.title("Comparação das Árvores")
+plt.ylabel("Score")
+plt.ylim(0, 1)
+plt.grid(axis="y")
+plt.show() 
+
+
+## plotar
+for config in arvores:
+
+    arvore = DecisionTreeClassifier(
+        criterion=config["criterion"],
+        max_depth=config["max_depth"],
+        min_samples_leaf=config["min_samples_leaf"],
+        min_samples_split=config["min_samples_split"],
+        random_state=42
+    )
+
+    arvore.fit(X_train, y_train)
+
+    
+    dot_data = export_graphviz(
+        arvore,
+        out_file=None,
+        feature_names=X.columns,
+        class_names=["Benigno", "Maligno"],
+        filled=True,
+        rounded=True,
+        special_characters=True
+    )
+
+    graph = graphviz.Source(dot_data)
+
+    nome_arquivo = config["nome"].replace(" ", "_")
+
+    graph.render(
+        filename=nome_arquivo,
+        format="pdf",
+        cleanup=True
+    )
+
+    y_pred = arvore.predict(X_test)
+
+    resultados.append({
+        "Árvore": config["nome"],
+        "Critério": config["criterion"],
+        "Profundidade": config["max_depth"],
+        "Min Leaf": config["min_samples_leaf"],
+        "Min Split": config["min_samples_split"],
+        "Accuracy": accuracy_score(y_test, y_pred),
+        "Precision": precision_score(y_test, y_pred),
+        "Recall": recall_score(y_test, y_pred)
+    })
+
+
+## matriz de confusão
+    cm = confusion_matrix(y_test, y_pred)
+
+    disp = ConfusionMatrixDisplay(
+        confusion_matrix=cm,
+        display_labels=["Benigno", "Maligno"]
+    )
+
+    disp.plot()
+
+    plt.title(f"Matriz de Confusão - {config['nome']}")
+
+    plt.savefig(
+        f"matriz_{config['nome'].replace(' ', '_')}.png",
+        bbox_inches="tight"
+    )
+
+    plt.close()
+
